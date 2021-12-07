46f487635ba8b3f3cd760c0789fe7d6e3f341898
apotdevin <potdevin.anthony@gmail.com>
2021-12-06 21:25:06 -0500

    chore: cleanup

--- src/client/server/schema/index.ts
+++ src/client/server/schema/index.ts
@@ -1,8 +1,6 @@
 import { merge } from 'lodash';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { generalTypes, queryTypes, mutationTypes } from './types';
-import { routeResolvers } from './route/resolvers';
-import { routeTypes } from './route/types';
 import { bosResolvers } from './bos/resolvers';
 import { bosTypes } from './bos/types';
 import { lnMarketsResolvers } from './lnmarkets/resolvers';
@@ -14,17 +12,11 @@ const typeDefs = [
   generalTypes,
   queryTypes,
   mutationTypes,
-  routeTypes,
   bosTypes,
   lnMarketsTypes,
   boltzTypes,
 ];
 
-const resolvers = merge(
-  routeResolvers,
-  bosResolvers,
-  lnMarketsResolvers,
-  boltzResolvers
-);
+const resolvers = merge(bosResolvers, lnMarketsResolvers, boltzResolvers);
 
 export const schema = makeExecutableSchema({ typeDefs, resolvers });
